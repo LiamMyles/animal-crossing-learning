2be03f41fe5139c54ca8fb478a8b94e3a7765f70
Liam Myles <liamthemonkey92@gmail.com>
2020-03-30 01:15:15 +0100

    Updates

---
+++ precache-manifest.ffbb86b861dc0f62bbd5223cd94f3710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45baf8e008b14dfc5c87e6af873b5380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a5b25e391475ad6aa4",
+    "url": "/static/css/main.b50b2e78.chunk.css"
+  },
+  {
+    "revision": "cef754aafd57460cf3f2",
+    "url": "/static/js/2.2028d085.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2028d085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a5b25e391475ad6aa4",
+    "url": "/static/js/main.b831e6db.chunk.js"
+  },
+  {
+    "revision": "ebaab4845f63682bebb2",
+    "url": "/static/js/runtime-main.05f9f558.js"
+  }
+]);
